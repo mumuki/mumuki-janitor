69673e5a49358a2c1c986dc0faf1e6932639dc52
Agustin Pina <aguspina87@gmail.com>
2016-12-09 19:06:52 -0300

    Add users update api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,6 @@
 module Api
   class UsersController < BaseController
+    before_action :set_user, only: :update
     def create
       user = User.create! user_params
       user.update_permissions! params[:user][:permissions]
@@ -7,8 +8,18 @@ module Api
       render json: { user: user.as_json }
     end
 
+    def update
+      @user.update! user_params.except(:email)
+      Mumukit::Nuntius::EventPublisher.publish 'UserChanged', user: @user.as_json
+      render json: { user: @user.as_json }
+    end
+
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email)
+    end
+
+    def set_user
+      @user = User.find_by(email: user_params[:email])
     end
     
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :organizations, only: [:index, :show, :edit, :create, :update]
   resources :users, only: [:index, :show, :edit, :create, :update]
   namespace :api do
-    resources :users, only: [:create, :update]
+    resources :users, only: [:create]
+    put '/users/' => 'users#update'
   end
 end
