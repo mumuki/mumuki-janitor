b4024520c30b0ecfedd46dae585d8c7fdb57ad74
Agustin Pina <aguspina87@gmail.com>
2016-12-09 19:13:05 -0300

    Add spec for users api

--- spec/controllers/users_api_controller_spec.rb
+++ spec/controllers/users_api_controller_spec.rb
@@ -1,20 +1,26 @@
 require 'rails_helper'
 
 describe Api::UsersController, type: :controller do
-  describe 'post' do
-    let(:user_json) do
-      {first_name: 'foo',
-       last_name: 'bar',
-       email: 'foo@bar.com',
-       permissions: {student: 'foo/*'}
-      }
-    end
-
+  let(:user_json) do
+    {first_name: 'foo',
+     last_name: 'bar',
+     email: 'foo@bar.com',
+     permissions: {student: 'foo/*'}
+    }
+  end
+  context 'post' do
     before { post :create, { user: user_json }.to_json}
 
     it { expect(response.status).to eq 200 }
     it { expect(User.count).to eq 1 }
     it { expect(User.first.permissions).to eq('student' => 'foo/*') }
+  end
+
+  context 'put' do
+    before { post :create, { user: user_json }.to_json}
+    before { put :update, { user: {email: 'foo@bar.com', first_name: 'Fede'} }.to_json}
+
+    it { expect(User.first.first_name).to eq('Fede') }
   end
 
 end
