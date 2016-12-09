81b51213824c5e39b1a6121dc1b5d62969861fc3
Agustin Pina <aguspina87@gmail.com>
2016-12-09 12:11:24 -0300

    Notify user changed event

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,6 +3,7 @@ module Api
     def create
       user = User.create! user_params
       user.update_permissions! params[:user][:permissions]
+      Mumukit::Nuntius::EventPublisher.publish 'UserChanged', user: user.as_json
       render json: { user: user.as_json }
     end
 
