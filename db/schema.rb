fb3a36a48bef6d3314796c8128661c6ea86808b2
Agustin Pina <aguspina87@gmail.com>
2016-12-12 11:45:30 -0300

    Add uid to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209012434) do
+ActiveRecord::Schema.define(version: 20161209234312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20161209012434) do
     t.text     "permissions", default: "{}", null: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
