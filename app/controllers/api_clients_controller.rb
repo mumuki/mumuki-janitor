66d8772bdd4e098f8cf7e0bff132bf0e79c4b193
Agustin Pina <aguspina87@gmail.com>
2016-12-13 15:52:23 -0300

    Add api_client controller actions

---
+++ app/controllers/api_clients_controller.rb
@@ -0,0 +1,16 @@
+class ApiClientsController < ApplicationController
+  def new
+    @api_client = ApiClient.new
+  end
+
+  def create
+    ApiClient.create! api_client_params
+    redirect_to :root
+  end
+
+  private
+
+  def api_client_params
+    params.require(:api_client).permit(:name, :permissions)
+  end
+end
