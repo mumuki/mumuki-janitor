576b04124b551b8cbc72cf12af43e5d9cb37bfe3
Agustin Pina <aguspina87@gmail.com>
2016-12-14 15:30:43 -0300

    Add nested user attributes to api client

--- app/controllers/api_clients_controller.rb
+++ app/controllers/api_clients_controller.rb
@@ -1,6 +1,8 @@
 class ApiClientsController < ApplicationController
+  before_action :parse_permissions, only: :create
   def new
     @api_client = ApiClient.new
+    @user = @api_client.build_user
   end
 
   def create
@@ -11,6 +13,10 @@ class ApiClientsController < ApplicationController
   private
 
   def api_client_params
-    params.require(:api_client).permit(:description, :permissions)
+    params.require(:api_client).permit(:description, user_attributes: [:first_name, :last_name, :email, permissions: Mumukit::Auth::Permissions.keys])
+  end
+
+  def parse_permissions
+    params[:api_client][:user_attributes].merge!(permissions: JSON.parse(params[:api_client][:user_attributes][:permissions]))
   end
 end
--- app/models/api_client.rb
+++ app/models/api_client.rb
@@ -5,11 +5,11 @@ class ApiClient < ApplicationRecord
   validates_presence_of :description
   before_create :set_token
   belongs_to :user
+  accepts_nested_attributes_for :user
 
   private
 
   def set_token
     self.token = Mumukit::Auth::Token.encode permissions
   end
-
 end
