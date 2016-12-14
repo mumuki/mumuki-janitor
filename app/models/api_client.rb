691e5fded5996779e8ea74bdc5f1dcee4b577136
Agustin Pina <aguspina87@gmail.com>
2016-12-14 18:44:38 -0300

    Separate callbacks in api client model

--- app/models/api_client.rb
+++ app/models/api_client.rb
@@ -2,9 +2,12 @@ class ApiClient < ApplicationRecord
 
   include WithPermissions
 
-  validates_presence_of :description
-  before_create :set_token
   belongs_to :user
+
+  before_create :set_token
+
+  validates_presence_of :description
+
   accepts_nested_attributes_for :user
 
   private
