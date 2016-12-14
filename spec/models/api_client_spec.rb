671e264a4a1e5ba6aee383e89722327b3a2f45b1
Agustin Pina <aguspina87@gmail.com>
2016-12-14 16:15:53 -0300

    Mock nuntius in test

--- spec/models/api_client_spec.rb
+++ spec/models/api_client_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 describe ApiClient, type: :model do
   let(:api_client) {create :api_client}
-  it { expect(api_client.token).to eq Mumukit::Auth::Token.encode(api_client.permissions) }
+  it { expect(api_client.token).to eq Mumukit::Auth::Token.generate_token }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
-
   config.use_transactional_fixtures = true
 
   config.infer_base_class_for_anonymous_controllers = false
