b2d4733c0a44a91e957532228fa4bd5d68adb0fc
Agustin Pina <aguspina87@gmail.com>
2016-12-14 18:55:16 -0300

    Introduce NotificationMode for nuntius

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   end
 
   def notify!
-    Mumukit::Nuntius::EventPublisher.publish 'UserChanged', user: self.as_json
+    NotificationMode.notify_event! 'UserChanged', user: self.as_json
   end
 
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,9 +7,6 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.before(:each) do
-    allow(Mumukit::Nuntius::EventPublisher).to receive(:publish)
-  end
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
