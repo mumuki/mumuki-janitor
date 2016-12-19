f93d1a3b755a49286c2468ba10b1ceb2835705c0
Agustin Pina <aguspina87@gmail.com>
2016-12-19 17:57:25 -0300

    Add api protection

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -1,6 +1,8 @@
 module Api
   class StudentsController < BaseController
+    before_action :set_slug, only: :create
     before_action :set_course, only: :create
+    before_action :protect!, only: :create
 
     def create
       @course.add_student!(user_params)
@@ -14,7 +16,15 @@ module Api
     end
 
     def set_course
-      @course = Course.find_by(slug: Mumukit::Auth::Slug.join_s(params.to_unsafe_h))
+      @course = Course.find_by(slug: @slug)
+    end
+
+    def set_slug
+      @slug = Mumukit::Auth::Slug.join_s params.to_unsafe_h
+    end
+
+    def protect!
+      @api_client.protect! :janitor, @slug
     end
 
   end
