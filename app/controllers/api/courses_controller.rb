bee29853dbae6402e92b4056acbeea2a5907e1a6
Agustin Pina <aguspina87@gmail.com>
2016-12-26 18:16:38 -0300

    Protect course api

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class CoursesController < BaseController
+    before_action :set_slug, only: :create
+    before_action :protect!, only: :create
 
     def create
       course = Course.create! course_params
@@ -10,6 +12,14 @@ module Api
 
     def course_params
       params.require(:course).permit(:slug, :code, :period, :description, :subscription_mode, shifts: [], days: [])
+    end
+
+    def set_slug
+      @slug = Mumukit::Auth::Slug.parse course_params[:slug]
+    end
+
+    def protect!
+      @api_client.protect! :janitor, @slug
     end
 
   end
--- spec/controllers/courses_api_controller_spec.rb
+++ spec/controllers/courses_api_controller_spec.rb
@@ -4,7 +4,7 @@ describe Api::CoursesController, type: :controller do
   before { set_api_client }
   let(:api_client) { create :api_client }
   let(:course_json) do
-    {slug: 'foo/bar',
+    {slug: 'test/bar',
      shifts: %w(morning),
      code: 'k2003',
      days: %w(monday wednesday),
@@ -13,14 +13,14 @@ describe Api::CoursesController, type: :controller do
      subscription_mode: 'closed'}
   end
 
-  let!(:organization) { create :organization, name: 'foo' }
+  let!(:organization) { create :organization, name: 'test' }
 
   context 'post' do
     before { post :create, params: { course: course_json }}
 
     it { expect(response.status).to eq 200 }
     it { expect(Course.count).to eq 1 }
-    it { expect(Course.first.uid).to eq 'foo/bar' }
+    it { expect(Course.first.uid).to eq 'test/bar' }
     it { expect(Course.first.subscription_mode).to eq SubscriptionMode::Closed }
     it { expect(Course.first.organization).to eq(organization) }
     it { expect(Course.first.shifts).to eq(%w(morning)) }
