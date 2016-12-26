c5ca0b928c209c32f257eacf390b2ca5b6fb6b2e
Agustin Pina <aguspina87@gmail.com>
2016-12-26 17:36:24 -0300

    Add dynamic errors

---
+++ app/controllers/concerns/with_dynamic_errors.rb
@@ -0,0 +1,26 @@
+module WithDynamicErrors
+  extend ActiveSupport::Concern
+
+  included do
+    unless Rails.application.config.consider_all_requests_local
+      rescue_from ActionController::RoutingError, with: :not_found
+    end
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from Mumukit::Auth::UnauthorizedAccessError, with: :forbidden
+  end
+
+  def not_found
+    render json: {}, status: 404
+  end
+
+  private
+
+  def internal_server_error(exception)
+    Rails.logger.error "Internal server error: #{exception} \n#{exception.backtrace.join("\n")}"
+    render json: {}, status: 500
+  end
+
+  def forbidden
+    render  json: {}, status: 403
+  end
+end
