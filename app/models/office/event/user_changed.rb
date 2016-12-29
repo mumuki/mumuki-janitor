1a69ed5030b01c2723f822391fe7e15704c3e8ed
Agustin Pina <aguspina87@gmail.com>
2016-12-29 17:20:10 -0300

    Remove unnecesary symbolize

--- app/models/office/event/user_changed.rb
+++ app/models/office/event/user_changed.rb
@@ -2,7 +2,7 @@ module Office
   module Event
     class UserChanged
       def self.execute!(payload)
-        User.import_from_json! payload.deep_symbolize_keys[:user]
+        User.import_from_json! payload[:user]
       end
     end
   end
