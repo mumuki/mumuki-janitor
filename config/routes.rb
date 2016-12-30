ba57d7f698958a96f14353d4a0a0d5f2ecbdc8c0
Agustin Pina <aguspina87@gmail.com>
2016-12-30 00:09:15 -0300

    Remove old route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: 'organizations#index'
   resources :organizations, only: [:index, :show, :edit, :create, :update]
   resources :users, only: [:index, :show, :edit, :create, :update]
-  #resources :api_clients, only: [:new, :create]
   namespace :api do
     resources :users, only: [:create, :update]
     resources :courses, only: [:create]
