0afd565fb84168728210a1a8ff82249f3dd19d3d
Federico Scarpa <fedescarpa@gmail.com>
2017-01-03 15:46:54 -0300

    format code

--- spec/controllers/students_api_controller_spec.rb
+++ spec/controllers/students_api_controller_spec.rb
@@ -3,26 +3,26 @@ require 'rails_helper'
 describe Api::StudentsController, type: :controller do
   before { set_api_client }
   let(:api_client) { create :api_client }
-  let(:student_json) {{
-    first_name: 'Agustin',
-    last_name: 'Pina',
-    email: 'agus@mumuki.org',
-    uid: 'agus@mumuki.org'
-  }}
+  let(:student_json) { {
+      first_name: 'Agustin',
+      last_name: 'Pina',
+      email: 'agus@mumuki.org',
+      uid: 'agus@mumuki.org'
+  } }
 
   let!(:organization) { create :organization, name: 'test' }
   let!(:course) { create :course }
 
   context 'post' do
-    let(:params) {{ student: student_json, organization: 'test', course: 'example' }}
+    let(:params) { {student: student_json, organization: 'test', course: 'example'} }
 
     context 'when user does not exist' do
 
-      before { post :create, params: params}
+      before { post :create, params: params }
 
       it { expect(response.status).to eq 200 }
       it { expect(User.count).to eq 2 }
-      it { expect(User.last.last_name).to eq'Pina' }
+      it { expect(User.last.last_name).to eq 'Pina' }
       it { expect(User.last.first_name).to eq 'Agustin' }
       it { expect(User.last.uid).to eq 'agus@mumuki.org' }
       it { expect(User.last.email).to eq 'agus@mumuki.org' }
@@ -32,14 +32,14 @@ describe Api::StudentsController, type: :controller do
     context 'when user exist' do
       context 'and have no permissions' do
         before { create :user, student_json }
-        before { post :create, params: params}
+        before { post :create, params: params }
         it { expect(response.status).to eq 200 }
         it { expect(User.count).to eq 2 }
         it { expect(User.last.permissions.student? 'test/example').to be true }
       end
       context 'and have permissions' do
-        before { create :user, student_json.merge(permissions: { student: 'foo/bar' }) }
-        before { post :create, params: params}
+        before { create :user, student_json.merge(permissions: {student: 'foo/bar'}) }
+        before { post :create, params: params }
         it { expect(response.status).to eq 200 }
         it { expect(User.count).to eq 2 }
         it { expect(User.last.permissions.student? 'test/example').to be true }
@@ -51,23 +51,23 @@ describe Api::StudentsController, type: :controller do
   end
 
   context 'attach student' do
-    let(:params) {{ uid: student_json[:uid], organization: 'test', course: 'example' }}
+    let(:params) { {uid: student_json[:uid], organization: 'test', course: 'example'} }
 
     context 'when user does not exist' do
-      before { post :attach, params: params}
+      before { post :attach, params: params }
       it { expect(response.status).to eq 404 }
     end
 
     context 'when user exist' do
       context 'and have no permissions' do
         before { create :user, student_json }
-        before { post :attach, params: params}
+        before { post :attach, params: params }
         it { expect(response.status).to eq 200 }
         it { expect(User.last.permissions.student? 'test/example').to be true }
       end
       context 'and have permissions' do
-        before { create :user, student_json.merge(permissions: { student: 'foo/bar' }) }
-        before { post :attach, params: params}
+        before { create :user, student_json.merge(permissions: {student: 'foo/bar'}) }
+        before { post :attach, params: params }
         it { expect(response.status).to eq 200 }
         it { expect(User.last.permissions.student? 'test/example').to be true }
         it { expect(User.last.permissions.student? 'foo/bar').to be true }
@@ -77,17 +77,17 @@ describe Api::StudentsController, type: :controller do
   end
 
   context 'detach student' do
-    let(:params) {{ uid: student_json[:uid], organization: 'test', course: 'example' }}
+    let(:params) { {uid: student_json[:uid], organization: 'test', course: 'example'} }
 
     context 'when user does not exist' do
-      before { post :detach, params: params}
+      before { post :detach, params: params }
       it { expect(response.status).to eq 404 }
     end
 
     context 'when user exist' do
       context 'and have permissions' do
-        before { create :user, student_json.merge(permissions: { student: 'test/example' }) }
-        before { post :detach, params: params}
+        before { create :user, student_json.merge(permissions: {student: 'test/example'}) }
+        before { post :detach, params: params }
         it { expect(response.status).to eq 200 }
         it { expect(User.last.permissions.student? 'test/example').to be false }
       end
