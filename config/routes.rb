c02637f9ae7b086568fb8220c0bf3b693a79ba74
Federico Scarpa <fedescarpa@gmail.com>
2017-01-05 10:34:02 -0300

    added uid contraint to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:create, :update], constraints: {id: /[^\/]+/}
     resources :courses, only: [:create]
-    '/courses/:organization/:course'.tap do |it|
-      post "#{it}/students" => 'students#create'
-      post "#{it}/students/:uid/attach" => 'students#attach'
-      post "#{it}/students/:uid/detach" => 'students#detach'
-      post "#{it}/teachers" => 'teachers#create'
-      post "#{it}/teachers/:uid/attach" => 'teachers#attach'
-      post "#{it}/teachers/:uid/detach" => 'teachers#detach'
+    constraints(uid: /[^\/]+/) do
+      '/courses/:organization/:course'.tap do |it|
+        post "#{it}/students" => 'students#create'
+        post "#{it}/students/:uid/attach" => 'students#attach'
+        post "#{it}/students/:uid/detach" => 'students#detach'
+        post "#{it}/teachers" => 'teachers#create'
+        post "#{it}/teachers/:uid/attach" => 'teachers#attach'
+        post "#{it}/teachers/:uid/detach" => 'teachers#detach'
+      end
     end
   end
 end
