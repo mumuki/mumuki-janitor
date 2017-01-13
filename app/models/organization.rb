9e6fdea02ebee4beb85d8f5523c02c9a8f5ea2cb
Rodrigo Alfonso <rodri042@gmail.com>
2017-01-13 14:21:01 -0300

    Validations over name

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,5 @@
 class Organization < ApplicationRecord
+  validates :name, :uniqueness => true, :format => { :with => /\A[-a-z0-9_]*\z/ }
   validates_presence_of :contact_email, :name, :locale
   validates :books, :length => { minimum: 1 }
   validates :locale, :inclusion => { :in => %w(es-AR en-US) }
