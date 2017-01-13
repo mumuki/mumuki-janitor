8c37596a1bd3f98453303f96bf5733563bab8d51
Rodrigo Alfonso <rodri042@gmail.com>
2017-01-13 04:18:07 -0300

    It can create organizations and notify the event

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -5,10 +5,18 @@ module Api
     before_action :protect!
 
     def create
-      render json: { test: @user }
+      organization = Organization.create! organization_params
+      organization.notify!
+      render json: organization
     end
 
     private
+
+    def organization_params
+      params.permit(:contact_email, :name, :locale, :description, :logo_url,
+                    :login_methods, :private, :theme_stylesheet, :terms_of_service,
+                    books: [])
+    end
 
     def set_user
       @user = User.find_by(id: @api_client.user_id)
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,9 @@
 class Organization < ApplicationRecord
+  validates_presence_of :contact_email, :name, :locale
+  validates :books, :length => { minimum: 1 }
+  validates :locale, :inclusion => { :in => %w(es-AR en-US) }
+
+  def notify!
+    Mumukit::Nuntius.notify_event!({organization: as_json}, 'OrganizationCreated')
+  end
 end
