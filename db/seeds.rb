aee7e0a1358037bb3df962f6692de20bc8171b54
Rodrigo Alfonso <rodri042@gmail.com>
2017-01-17 18:27:22 -0300

    Fixed the seed to pass validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,5 @@ Organization.create! name: 'academy',
                      contact_email: 'info@mumuki.org',
                      private: false,
                      books: ['mumuki/mumuki-libro-programacion'],
+                     login_methods: ['facebook', 'twitter', 'google', 'github'],
                      locale: 'es-AR'
