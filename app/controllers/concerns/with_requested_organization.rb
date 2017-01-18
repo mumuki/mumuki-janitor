b3adcf3d7955ee28d8732178bf88b796123b3d97
Rodrigo Alfonso <rodri042@gmail.com>
2017-01-17 23:55:48 -0300

    Removed wrong copied code

--- app/controllers/concerns/with_requested_organization.rb
+++ app/controllers/concerns/with_requested_organization.rb
@@ -1,14 +1,6 @@
 module WithRequestedOrganization
   extend ActiveSupport::Concern
 
-  included do
-    unless Rails.application.config.consider_all_requests_local
-      rescue_from ActionController::RoutingError, with: :not_found
-    end
-    rescue_from ActiveRecord::RecordNotFound, with: :not_found
-    rescue_from Mumukit::Auth::UnauthorizedAccessError, with: :forbidden
-  end
-
   def requested_organization
     Organization.find_by_name(id_param) || raise(ActiveRecord::RecordNotFound)
   end
