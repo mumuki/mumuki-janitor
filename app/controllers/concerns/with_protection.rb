61f891bfd7e3fee360e4f2615c8f2cdc1b643a51
Rodrigo Alfonso <rodri042@gmail.com>
2017-01-19 14:31:25 -0300

    Using Mumukit::Auth method to extract header

--- app/controllers/concerns/with_protection.rb
+++ app/controllers/concerns/with_protection.rb
@@ -12,6 +12,7 @@ module WithProtection
   private
 
   def set_api_client
-    @api_client = ApiClient.find_by!(token: authorization_header.split(' ').last)
+
+    @api_client = ApiClient.find_by! token: Mumukit::Auth::Token.extract_from_header(authorization_header)
   end
 end
