d98f5d0d254362a4fd8f83aa19854743d15a07cc
Federico Scarpa <fedescarpa@gmail.com>
2017-01-27 00:27:13 -0300

    added tags input... needs styling

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'mumukit-auth', '~> 5.0'
 gem 'mumukit-core', '~> 0.5'
 
 gem 'nprogress-rails'
+gem 'bootstrap-tagsinput-rails'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-mumuki-styles', '1.1.0'
--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -7,31 +7,64 @@ function addPermission() {
     var organization = $('#permission_organization').val();
     var content = $('#permission_content').val();
     var grant = $role.val() === 'owner' ? '*' : organization + '/' + content;
-    addGrantToRole($role, grant)
+    addGrantToRole($role, grant);
+    addFaTimes();
+}
+
+function removePermission(e) {
+    var target = e.target;
+    var grantToRemove = e.item;
+
+    var fieldset = $(target).parents('[id^="fieldset_"]');
+    var grantsInput = fieldset.find('input[data-role="tagsinput"]');
+
+    var grants = grantsInput.val().split(':');
+
+    var newGrants = grants.filter(function (g) {
+        return g !== grantToRemove;
+    });
+    grantsInput.val(newGrants.join(':'));
+
+    if (newGrants.length === 0) fieldset.remove();
+}
+
+function addGrant(grantsInput, grant) {
+    var separator = ':';
+    var grants = grantsInput.get(0).value.split(separator);
+    if (!hasGrant(grants, grant)) {
+        grants.push(grant);
+        addGrantTag(grantsInput, grant);
+    }
+    grantsInput.val(grants.join(separator));
+}
+
+function addGrantTag(grantsInput, grant) {
+    var bootstrapTagsInput = grantsInput.parent().find('.bootstrap-tagsinput > input');
+    bootstrapTagsInput.val(grant);
+    var e = jQuery.Event("keypress");
+    e.which = 13;
+    e.keyCode = 13;
+    bootstrapTagsInput.trigger(e);
 }
 
 function addGrantToRole($role, grant) {
-    var separator = ':';
     var grantsInput = $('input[name="user[permissions][' + $role.val() + ']"]');
 
-    if (grantsInput.length !== 0) {
-        var grants = grantsInput.get(0).value.split(separator);
-        if (!hasGrant(grants, grant)) grants.push(grant);
-        grantsInput.val(grants.join(separator));
-    } else {
-        addInput($role, grant);
-    }
+    grantsInput.length !== 0 ? addGrant(grantsInput, grant) : addInput($role, grant);
 }
 
 function addInput($role, grant) {
     var rolesFieldSet = [
-        '<fieldset class="fieldset_' + $role.val() + '">',
+        '<fieldset id="fieldset_' + $role.val() + '">',
         '<label for="">' + $role.find(':selected').text() + '</label>',
-        '<input type="text" name="user[permissions][' + $role.val() + ']" value="' + grant + '" class="form-control" readonly>',
+        '<input type="text" name="user[permissions][' + $role.val() + ']" value="' + grant + '" class="form-control" readonly data-role="tagsinput">',
         '</fieldset>'
     ].join('\n');
 
     $('#permissions_list').append(rolesFieldSet);
+    var input = $('input[name="user[permissions][' + $role.val() + ']"]');
+    input.tagsinput();
+    input.on('beforeItemRemove', removePermission);
 }
 
 function hasGrant(grants, grant) {
@@ -39,3 +72,13 @@ function hasGrant(grants, grant) {
         return g === grant;
     });
 }
+
+function addFaTimes() {
+    var spans = $('span[data-role="remove"]:not(:has(i))');
+    spans.append('<i class="fa fa-fw fa-times" style="cursor: pointer"></i>');
+}
+
+$(document).ready(function () {
+    addFaTimes();
+    $('input').on('beforeItemRemove', removePermission);
+});
