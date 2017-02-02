241671428f52db663879bbe82062d92d11a43340
Rodrigo Alfonso <rodri042@gmail.com>
2017-02-02 15:57:02 -0300

    Avoiding Organization.last

--- spec/controllers/organizations_static_assets_spec.rb
+++ spec/controllers/organizations_static_assets_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe 'Static assets', type: :controller do
   let!(:public_path) { Rails.public_path.to_s }
-  let(:organization) { Organization.last }
+  let(:organization) { Organization.find_by name: 'assets' }
 
   context 'SCSS' do
     let!(:scss) { "$font-stack:    Helvetica, sans-serif;\n$primary-color: #333;\n\nbody {\n\n  font: 100% $font-stack;\n  color: $primary-color;\n}" }
     let!(:css) { "body {\n  font: 100% Helvetica, sans-serif;\n  color: #333; }\n" }
     let(:path) { "#{public_path}/#{organization.theme_stylesheet_url}" }
-    before { create :organization, theme_stylesheet: scss }
+    before { create :organization, name: 'assets', theme_stylesheet: scss }
 
     it { expect(File.read path).to eq css }
   end
@@ -16,7 +16,7 @@ describe 'Static assets', type: :controller do
   context 'JavaScript' do
     let!(:javascript) { "window.a = function() { }" }
     let(:path) { "#{public_path}/#{organization.extension_javascript_url}" }
-    before { create :organization, extension_javascript: javascript}
+    before { create :organization, name: 'assets', extension_javascript: javascript}
 
     it { expect(File.read path).to eq javascript }
   end
