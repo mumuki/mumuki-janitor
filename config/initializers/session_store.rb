c2dbb57131d223a48cd1bc6817901cc41c4f55eb
Franco Bulgarelli <franco@mumuki.org>
2017-02-09 10:08:23 -0300

    Setting proper domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_office_session'
+Rails.application.config.session_store :cookie_store,
+                                       key: '_mumuki_office_session',
+                                       domain: Rails.application.config.cookies_domain
+
