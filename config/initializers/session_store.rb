79e94dc79ec74fb1edd0520e1bea66ad4317e2d0
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-02-09 14:31:27 -0300

    Removing subdomain cookies handling by now

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store,
-                                       key: '_mumuki_office_session',
-                                       domain: Rails.application.config.cookies_domain
-
+                                       key: '_mumuki_office_session'
