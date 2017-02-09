fbb3174a08402a7909c47f8bbe578586dcb65e6d
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-02-09 14:25:41 -0300

    Using mumukit login in api too

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,33 +1,19 @@
 module Api
   class BaseController < ActionController::Base
+    Mumukit::Login.configure_controller! self
+
     protect_from_forgery with: :null_session
 
     include WithCustomUnhandledErrors
     include WithDynamicErrors
     include WithAuthorization
 
-    before_action :verify_token!, :set_api_client!
+    before_action :verify_api_client!
 
     private
 
-    def encoded_token
-      @encoded_token ||= Mumukit::Auth::Token.extract_from_header(authorization_header)
-    end
-
-    def verify_token!
-      Mumukit::Auth::Token.decode(encoded_token).verify_client!
-    end
-
-    def set_api_client!
-      @api_client = ApiClient.find_by! token: encoded_token
-    end
-
-    def current_user
-      @api_client.user
-    end
-
-    def authorization_header
-      request.env['HTTP_AUTHORIZATION']
+    def verify_api_client!
+      ApiClient.verify_token! Mumukit::Auth::Token.extract_from_header(request.env['HTTP_AUTHORIZATION'])
     end
   end
 end
