c7d34e76de9961459f0bc8bcb643624d7739db98
Agustin Pina <aguspina87@gmail.com>
2017-04-05 19:16:07 -0300

    Fix events

--- lib/events.rb
+++ lib/events.rb
@@ -1,9 +1,9 @@
 Mumukit::Nuntius::EventConsumer.handle do
-  event :UserChanged do |payload|
+  event 'UserChanged' do |payload|
     User.import_from_json! payload[:user]
   end
 
-  event :CourseChanged do |payload|
+  event 'CourseChanged' do |payload|
     Course.import_from_json! payload[:course]
   end
 end
