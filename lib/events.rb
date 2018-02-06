a90b75b3ac8098ac3a6d58cb7ae6549f486131c6
Federico Scarpa <fedescarpa@gmail.com>
2018-02-06 16:58:43 -0300

    removed some fields from events

--- lib/events.rb
+++ lib/events.rb
@@ -1,9 +1,9 @@
 Mumukit::Nuntius::EventConsumer.handle do
   event 'UserChanged' do |payload|
-    User.import_from_json! payload[:user]
+    User.import_from_json! payload[:user].except(:created_at, :updated_at)
   end
 
   event 'CourseChanged' do |payload|
-    Course.import_from_json! payload[:course]
+    Course.import_from_json! payload[:course].except(:created_at, :updated_at)
   end
 end
