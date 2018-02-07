3e037f0ce3161f2bec27c84dfd6ec400d46ee7ae
Franco Bulgarelli <franco@mumuki.org>
2018-02-07 11:00:29 -0300

    Ensuring that users import accepts nil permissions

--- spec/models/user_changed_spec.rb
+++ spec/models/user_changed_spec.rb
@@ -1,15 +1,33 @@
 require 'spec_helper'
 
 describe 'UserChanged' do
-  let(:user_json) { {
+  before { User.import_from_json! user_json }
+
+  shared_context 'a proper import' do
+    it { expect(User.first.uid).to eq 'foo@bar.com' }
+    it { expect(User.first.first_name).to eq 'Foo' }
+    it { expect(User.first.last_name).to eq 'Bar' }
+  end
+
+  context 'with permissions changes' do
+    let(:user_json) { {
       uid: 'foo@bar.com',
       first_name: 'Foo',
       last_name: 'Bar',
       permissions: {student: 'test/example'}
-  } }
-  before { User.import_from_json! user_json }
-  it { expect(User.first.uid).to eq 'foo@bar.com' }
-  it { expect(User.first.first_name).to eq 'Foo' }
-  it { expect(User.first.last_name).to eq 'Bar' }
-  it { expect(User.first.permissions.student? 'test/example').to be true }
+    } }
+
+    it_behaves_like 'a proper import'
+    it { expect(User.first.permissions.student? 'test/example').to be true }
+  end
+
+  context 'no permissions changed' do
+    let(:user_json) { {
+        uid: 'foo@bar.com',
+        first_name: 'Foo',
+        last_name: 'Bar'
+    } }
+
+    it_behaves_like 'a proper import'
+  end
 end
